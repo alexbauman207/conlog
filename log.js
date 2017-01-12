e8939c5440ee9d1c12c93342e31267403097b766
arbauman <arbauman207@gmail.com>
2017-01-12 04:54:21 -0500

    Revision
    No longer commits outages less than a minute
    Changed naming scheme for better readability in Rethink
    Updated API to order results chronologically

--- log.js
+++ log.js
@@ -8,47 +8,55 @@ const thinky = require('thinky')({
 });
 const isConnectivity = new IsConnected();
 let start = "";
-let end = "";
+let stop = "";
 
 routes = router.get('/', (req, res) => {
-  thinky.r.table('Disconnect').run((conn, results) => {
+  thinky.r.table('Disconnect').orderBy('start').run((conn, results) => {
     res.status(200).json(results);
   });
 })
 
 app.use('/api/Disconnects', routes)
-app.listen(8080, () => {
-  console.log('App listening on port 8080');
+app.listen(8020, () => {
+  console.log('App listening on port 8020');
 })
 
 // Model
 const Disconnect = thinky.createModel('Disconnect', {
-  end: Date,
+  stop: Date,
   start: Date
 });
 console.log("Beginning Internet check");
 
 // Events
 isConnectivity.on('connected', function connected() {
-  console.log(`${new Date().toLocaleTimeString()} Internet Connected`);
-  if (start !== "" && end === "") {
-    let disconnect = new Disconnect({
-      start: start,
-      end: new Date()
-    });
-    disconnect.saveAll().then(function(result) {
-      console.log(`${new Date().toLocaleTimeString()} Successfully saved to database with ID ${result.id}`);
-    });
-    start = "";
-    end = "";
+  if(start !== "" && stop === "") {
+    stop = new Date();
+    if(stop.getTime() - start.getTime() <= 60000) {
+      console.log(`${new Date().toLocaleTimeString()} Outages under a minute are ignored.`)
+      return;
+    } else {
+      let disconnect = new Disconnect({
+        start: start,
+        stop: new Date()
+      });
+      disconnect.saveAll().then(function(result) {
+        console.log(`${new Date().toLocaleTimeString()} Saved to database with ID ${result.id}`);
+      });
+      start = "";
+      stop = "";
+    }
+  } else {
+    console.log(`${new Date().toLocaleTimeString()} Internet Connected`);
   }
 });
 
 isConnectivity.on('disconnected', function disconnected() {
-  console.log(`${new Date().toLocaleTimeString()} Internet Not Connected`);
-  if (start === "") {
+  if(start === "") {
     start = new Date();
     console.log(`${new Date().toLocaleTimeString()} Disconnected, time recorded.`);
+  } else {
+    console.log(`${new Date().toLocaleTimeString()} Internet Not Connected`);
   }
 });
 
